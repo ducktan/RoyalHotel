b22dd3c4b675e41f8a5263e32808179294d4db0a
ducktan <22521303@gm.uit.edu.vn>
2024-06-10 10:29:55 +0700

    merge chat

--- Chat.Designer.cs
+++ Chat.Designer.cs
@@ -29,31 +29,41 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cbID = new System.Windows.Forms.ComboBox();
             this.label6 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.kryptonButton5 = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            this.btnSendImg = new System.Windows.Forms.Button();
+            this.RoleBox = new System.Windows.Forms.TextBox();
+            this.chatRTB = new System.Windows.Forms.RichTextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.kryptonButton1 = new ComponentFactory.Krypton.Toolkit.KryptonButton();
             this.label2 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.cbID);
             this.groupBox1.Controls.Add(this.label6);
-            this.groupBox1.Controls.Add(this.textBox1);
             this.groupBox1.Controls.Add(this.kryptonButton5);
             this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.ForeColor = System.Drawing.Color.SteelBlue;
-            this.groupBox1.Location = new System.Drawing.Point(31, 80);
+            this.groupBox1.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.groupBox1.Location = new System.Drawing.Point(16, 80);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(223, 193);
-            this.groupBox1.TabIndex = 270;
+            this.groupBox1.TabIndex = 287;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "TÌM KIẾM";
+            // 
+            // cbID
+            // 
+            this.cbID.DropDownWidth = 270;
+            this.cbID.FormattingEnabled = true;
+            this.cbID.Location = new System.Drawing.Point(18, 77);
+            this.cbID.Name = "cbID";
+            this.cbID.Size = new System.Drawing.Size(179, 33);
+            this.cbID.TabIndex = 261;
             // 
             // label6
             // 
@@ -67,100 +77,111 @@
             this.label6.TabIndex = 259;
             this.label6.Text = "▶ Mã nhân viên";
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(18, 77);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(179, 31);
-            this.textBox1.TabIndex = 261;
-            // 
             // kryptonButton5
             // 
             this.kryptonButton5.AccessibleRole = System.Windows.Forms.AccessibleRole.OutlineButton;
             this.kryptonButton5.ButtonStyle = ComponentFactory.Krypton.Toolkit.ButtonStyle.Gallery;
             this.kryptonButton5.Location = new System.Drawing.Point(18, 126);
             this.kryptonButton5.Name = "kryptonButton5";
-            this.kryptonButton5.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.Office2010Blue;
             this.kryptonButton5.Size = new System.Drawing.Size(179, 36);
+            this.kryptonButton5.StateCommon.Back.Color1 = System.Drawing.Color.DodgerBlue;
+            this.kryptonButton5.StateCommon.Back.Color2 = System.Drawing.Color.DodgerBlue;
+            this.kryptonButton5.StateCommon.Content.ShortText.Color1 = System.Drawing.Color.White;
+            this.kryptonButton5.StateCommon.Content.ShortText.Color2 = System.Drawing.Color.White;
             this.kryptonButton5.StateCommon.Content.ShortText.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.kryptonButton5.TabIndex = 260;
             this.kryptonButton5.UseWaitCursor = true;
             this.kryptonButton5.Values.Text = "SEARCH";
             // 
+            // btnSendImg
+            // 
+            this.btnSendImg.Location = new System.Drawing.Point(1024, 408);
+            this.btnSendImg.Name = "btnSendImg";
+            this.btnSendImg.Size = new System.Drawing.Size(26, 23);
+            this.btnSendImg.TabIndex = 292;
+            this.btnSendImg.Text = "|||";
+            this.btnSendImg.UseVisualStyleBackColor = true;
+            this.btnSendImg.Click += new System.EventHandler(this.btnSendImg_Click);
+            // 
+            // RoleBox
+            // 
+            this.RoleBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RoleBox.Location = new System.Drawing.Point(16, 328);
+            this.RoleBox.Name = "RoleBox";
+            this.RoleBox.ReadOnly = true;
+            this.RoleBox.Size = new System.Drawing.Size(223, 27);
+            this.RoleBox.TabIndex = 291;
+            // 
+            // chatRTB
+            // 
+            this.chatRTB.Location = new System.Drawing.Point(282, 32);
+            this.chatRTB.Name = "chatRTB";
+            this.chatRTB.Size = new System.Drawing.Size(767, 370);
+            this.chatRTB.TabIndex = 290;
+            this.chatRTB.Text = "";
+            // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.SteelBlue;
-            this.label1.Location = new System.Drawing.Point(25, 32);
+            this.label1.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label1.Location = new System.Drawing.Point(10, 32);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(158, 31);
-            this.label1.TabIndex = 268;
+            this.label1.TabIndex = 285;
             this.label1.Text = "TRÒ CHUYỆN";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(297, 290);
+            this.textBox3.Location = new System.Drawing.Point(282, 408);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(649, 22);
-            this.textBox3.TabIndex = 274;
+            this.textBox3.Size = new System.Drawing.Size(736, 22);
+            this.textBox3.TabIndex = 289;
             // 
             // kryptonButton1
             // 
             this.kryptonButton1.AccessibleRole = System.Windows.Forms.AccessibleRole.OutlineButton;
             this.kryptonButton1.ButtonStyle = ComponentFactory.Krypton.Toolkit.ButtonStyle.Gallery;
-            this.kryptonButton1.Location = new System.Drawing.Point(297, 324);
+            this.kryptonButton1.Location = new System.Drawing.Point(282, 436);
             this.kryptonButton1.Name = "kryptonButton1";
             this.kryptonButton1.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.Office2010Blue;
             this.kryptonButton1.Size = new System.Drawing.Size(110, 36);
+            this.kryptonButton1.StateCommon.Back.Color1 = System.Drawing.Color.DodgerBlue;
+            this.kryptonButton1.StateCommon.Back.Color2 = System.Drawing.Color.DodgerBlue;
+            this.kryptonButton1.StateCommon.Content.ShortText.Color1 = System.Drawing.Color.White;
+            this.kryptonButton1.StateCommon.Content.ShortText.Color2 = System.Drawing.Color.White;
             this.kryptonButton1.StateCommon.Content.ShortText.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.kryptonButton1.TabIndex = 269;
+            this.kryptonButton1.TabIndex = 286;
             this.kryptonButton1.UseWaitCursor = true;
             this.kryptonButton1.Values.Text = "SEND";
+            this.kryptonButton1.Click += new System.EventHandler(this.kryptonButton1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.SteelBlue;
-            this.label2.Location = new System.Drawing.Point(25, 281);
+            this.label2.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label2.Location = new System.Drawing.Point(10, 281);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(102, 31);
-            this.label2.TabIndex = 272;
+            this.label2.TabIndex = 288;
             this.label2.Text = "VAI TRÒ";
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "NHÂN VIÊN",
-            "QUẢN LÝ"});
-            this.comboBox1.Location = new System.Drawing.Point(31, 324);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(222, 24);
-            this.comboBox1.TabIndex = 271;
-            // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Location = new System.Drawing.Point(297, 32);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(649, 241);
-            this.richTextBox1.TabIndex = 275;
-            this.richTextBox1.Text = "";
             // 
             // Chat
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(970, 386);
-            this.Controls.Add(this.richTextBox1);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1061, 505);
             this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnSendImg);
+            this.Controls.Add(this.RoleBox);
+            this.Controls.Add(this.chatRTB);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.kryptonButton1);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.comboBox1);
+            this.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.Name = "Chat";
             this.Text = "Chat";
@@ -174,14 +195,15 @@
         #endregion
 
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cbID;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox textBox1;
         private ComponentFactory.Krypton.Toolkit.KryptonButton kryptonButton5;
+        private System.Windows.Forms.Button btnSendImg;
+        private System.Windows.Forms.TextBox RoleBox;
+        private System.Windows.Forms.RichTextBox chatRTB;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox3;
         private ComponentFactory.Krypton.Toolkit.KryptonButton kryptonButton1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
